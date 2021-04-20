8c1c737651fe940747171d48c64b7deeac1048e2
captain nash <holyshit_2012@163.com>
2021-04-20 19:33:48 +1000

    using latest kotlin module to build and push docker image (#3)
    * docker build
    
    * available version
    
    * Update settings.kts
    
    modify Base class
    
    * Update settings.kts
    
    * new artifactory
    
    * modify settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import com.kotlin.v2019_2.buildTypes.DockerBuild
 
@@ -12,21 +13,37 @@ version = "2020.2"
 
 
 project {
-    params {
-        // This makes it impossible to change the build settings through the UI
-        param("teamcity.ui.settings.readOnly", "true")
-    }
 
     vcsRoot(VCSExample)
+
+
+    buildType(DockerBuildsteve)
+
 }
+
+object DockerBuildsteve : DockerBuild({
+    name = "docker build"
+
+    vcs {
+        root(VCSExample)
+    }
+
+
+    repository = "13.239.84.133:8082"
+    imageName = "filepizza"
+    tag = "v1.0.4"
+    artusername = "admin"
+    artpassword = "credentialsJSON:4365953f-8420-45b5-bebe-924c1243cd0a"
+
+})
 
 
 object VCSExample : GitVcsRoot({
     name = "VCSExample"
     url = "https://github.com/freefox-do/filepizza.git"
-    branch = "refs/heads/master"
+    branch = "refs/heads/steve"
     authMethod = password {
-        userName = "wellyfox"
-        password = "zxx010cfab59641ee19092b8307a116427c6ae958a7948f59e971b95cf982c6f2c5d196fb90892f7a6f775d03cbe80d301b"
+        userName = "capnash"
+        password = "d5bbe75793d3b650338fee279857bb1e90e8ea28"
     }
 })
