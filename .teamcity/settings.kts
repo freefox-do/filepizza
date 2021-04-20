21b634d1b34da101acfc0ee88b7bd8f068a75d9c
captain nash <holyshit_2012@163.com>
2021-04-20 19:47:36 +1000

    Update settings.kts (#4)
    update CVS branch

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,7 @@ object DockerBuildsteve : DockerBuild({
 object VCSExample : GitVcsRoot({
     name = "VCSExample"
     url = "https://github.com/freefox-do/filepizza.git"
-    branch = "refs/heads/steve"
+    branch = "refs/heads/main"
     authMethod = password {
         userName = "capnash"
         password = "d5bbe75793d3b650338fee279857bb1e90e8ea28"
